ebc2edce673ad6f7227d5f83b7fb44ab24f7501e
sinithwar <sinithwar@yahoo.com>
2016-12-08 09:52:08 -0500

    Added Basic Functionality Added Fire Recipes Activity Added Herbs Activity Added Fire Recipes Content Added Herbs Content

---
+++ app/src/main/java/dowlingandlainandtommymertell/chinesemedicinalapplication/FiveElementDietActivity.java
@@ -0,0 +1,13 @@
+package dowlingandlainandtommymertell.chinesemedicinalapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FiveElementDietActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_element_diet);
+    }
+}
--- app/src/main/java/dowlingandlainandtommymertell/chinesemedicinalapplication/LandingPage.java
+++ app/src/main/java/dowlingandlainandtommymertell/chinesemedicinalapplication/LandingPage.java
@@ -1,7 +1,10 @@
 package dowlingandlainandtommymertell.chinesemedicinalapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        TextView herbs = (TextView) findViewById(R.id.herbs_link);
+        TextView diets = (TextView) findViewById(R.id.diets_link);
+
+
+        herbs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent herb_link = new Intent(LandingPage.this, HerbsActivity.class);
+                startActivity(herb_link);
+            }
+        });
+
+        diets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent diets_link = new Intent(LandingPage.this, FireRecipesActivity.class);
+                startActivity(diets_link);
+            }
+        });
     }
 }
